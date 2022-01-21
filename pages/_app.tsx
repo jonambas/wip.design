2b76d3cecf0a58a3d2efaefbc4b14bcb670989f1
Jon Ambas <jon@jonambas.com>
2022-01-20 20:09:38 -0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,59 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Theme from '@sweatpants/theme';
+import Box from '@sweatpants/box';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  @font-face {
+    font-family: 'Whyte-Inktrap';
+    src: url('./ABCWhyteInktrap-Bold-Trial.woff2') format('woff2'),
+      url('./ABCWhyteInktrap-Bold-Trial.woff') format('woff');
+    font-weight: 800;
+    font-style: bold;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Theme
+      theme={{
+        space: {
+          0: '0rem',
+          100: '0.25rem',
+          200: '0.5rem',
+          300: '0.75rem',
+          400: '1rem',
+          500: '1.5rem',
+          600: '2rem',
+          700: '3rem',
+          800: '4rem',
+          900: '6rem',
+          1000: '8rem',
+        },
+        colors: {
+          blue: '#0000FF',
+          black: '#000',
+        },
+        fonts: {
+          sans: "'Whyte-Inktrap', -apple-system, BlinkMacSystemFont, 'San Francisco', 'Segoe UI', Roboto, Helvetica, sans-serif;",
+          mono: "'PT Mono', 'SFMono-Regular', Monaco, Consolas, 'Lucida Console', monospace",
+        },
+      }}
+    >
+      <GlobalStyle />
+      <Box m={['500', '600', '800']}>
+        <Component {...pageProps} />
+      </Box>
+    </Theme>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,61 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" type="image/png" href="/favicon.png" />
+          {/* <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-29511296-7"
+          ></script> */}
+          {/* <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-29511296-7');`,
+            }}
+          ></script> */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
