0deac841096e7e19437e4e3495e85c4388e6f1ee
jonambas <jon@jonambas.com>
2023-07-24 19:33:38 -0400

    chore: update content

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,16 +19,6 @@ const Home: NextPage = () => {
         <Box mb="900" display="grid" gridTemplateColumns={['1fr', '1fr 1fr']}>
           <div />
           <Box color="black" fontFamily="mono" maxWidth="20rem">
-            <Box mb="400">
-              Welcome to WIP Design – the creative studio of Jon Ambas. Established in 2022 and
-              based in Baltimore, Maryland.
-            </Box>
-            <Box mb="400">
-              Our mission is to deliver bespoke design and engineering solutions with the highest
-              level of craftmanship for our clients and create unique artwork to share with the
-              world.
-            </Box>
-            <Box mb="400">We create graphics, design systems, and digital products.</Box>
             <p>jon@wip.design.</p>
           </Box>
         </Box>
